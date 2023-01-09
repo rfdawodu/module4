c8020e9a26e7aea0bad5903f8da7caf2a8e790e2
rfdawodu <rfdawodu1@gmail.com>
2023-01-09 10:31:00 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,16 @@
+const names = ["john", "jeml", "jackel", "meg"];
+console.log(names);
+
+const contJ = names.filter((name) => name.includes("j"));
+console.log("Goodbye JSomeName");
+
+const contN = names.filter((name) => name == "");
+console.log("Hello SomeName");
+
+// let pattern = /jo/gi;
+// for (const name of names) {
+//   //   console.log(name);
+//   if (name == pattern) {
+//     console.log("JsomeNmae");
+//   }
+// }
